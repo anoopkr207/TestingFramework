225bca8d5bcbda4b08ab1002f7412ac462deac62
Anoop Kumar <anoop.kumar@unthinkable.co>
2021-04-30 14:42:52 +0530

    Finished our testing framework

---
+++ TestingFramework/TestingFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestingFramework
+//
+//  Created by daffolapmac146 on 30/04/21.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init () {}
+    
+    public static func doSomething() -> String {
+        return "This is an example of private pod and install using cocoapods"
+    }
+}
